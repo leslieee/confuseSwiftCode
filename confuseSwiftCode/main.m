3386f25e37644b0dbc5654286fb678fd0301a0cb
leslie <hushuangsoutheastasia@gmail.com>
2018-12-06 09:36:53 +0800

    Initial Commit

---
+++ confuseSwiftCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  confuseSwiftCode
+//
+//  Created by IMAC on 2018/12/6.
+//  Copyright © 2018年 leslie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
